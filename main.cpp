6f74f4f50250c413073dd8189d5ea5394b28b4ef
jianchao ji <32049203+jianchaoji@users.noreply.github.com>
2019-10-30 14:07:18 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+
+//#include"readFile.h"
+#include"Matching.h"
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+int  main(int arc,char *arv[] ) {
+	
+	int choice;
+ 	int i=1;
+	char *inputFile;
+ 	inputFile=arv[i];
+	matching test(inputFile);
+	//match test1;
+	
+	test.readFile(inputFile);
+	//test.writeFile();
+	//test.reReadFile();
+	//cout << "Doing pre now " << endl;
+	test.pre();
+        test.handShaking(inputFile);
+	//cout << "Doing verify now " << endl;
+	//test.verify();
+	//test1.tree();
+	//test.treeDepth();
+	//test.counting(inputFile);
+	/*for (int i = 0; i < 50; i++) {
+		test.maxWeight();
+		test.matchWeight();
+	}*/
+	//test.deleteWeight();
+	//test.maxWeight();
+	//test.matchWeight();
+	//system("pause");
+	return 0;
+}
